a5d1bbd2ccb7741a64832f9eb4a2c6bd20d71e5e
Jarod <schuppjarod@gmail.com>
2022-11-27 17:42:22 -0500

    Complete Flexbox template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridRow: {
+        'span-10': 'span 10 / span 10',
+      }
+    },
   },
   plugins: [],
 }
